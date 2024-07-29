44dfbec04e8ff6f61a5977a8ebd3036d28ecb68f
Hugo LE BLEVEC <hugo.leblevec@gmail.com>
2024-07-29 16:46:27 +0200

    adding sources for hw build

---
+++ hardware/build.py
@@ -0,0 +1,59 @@
+import finn.builder.build_dataflow as build
+import finn.builder.build_dataflow_config as build_cfg
+from custom_steps import *
+import argparse
+
+build_dataflow_steps = [
+    "step_qonnx_to_finn",
+    "step_tidy_up",
+    custom_step_streamline,
+    custom_step_convert_to_hw,
+    "step_create_dataflow_partition",
+    "step_specialize_layers",
+    "step_target_fps_parallelization",
+    "step_minimize_bit_width",
+    "step_apply_folding_config",
+    "step_generate_estimate_reports",
+    "step_hw_codegen",
+    "step_hw_ipgen",
+    "step_set_fifo_depths",
+    "step_create_stitched_ip",
+    "step_measure_rtlsim_performance",
+    "step_out_of_context_synthesis",
+    "step_synthesize_bitfile",
+    "step_make_pynq_driver",
+    "step_deployment_package",
+]
+
+def main(model, output, folding_config_file):
+    
+
+    cfg = build.DataflowBuildConfig(
+    output_dir          = output,
+    synth_clk_period_ns = 3.3,
+    target_fps          = 30,
+    board               = "TySOM-3A-ZU19EG",
+    # board               = "ZCU104",
+    shell_flow_type     = build_cfg.ShellFlowType.VIVADO_ZYNQ,
+    steps               = build_dataflow_steps,
+    folding_config_file = folding_config_file,
+    split_large_fifos = True,
+    auto_fifo_depths = False,
+    large_fifo_mem_style = build_cfg.LargeFIFOMemStyle.URAM,
+    # force_rtl_conv_inp_gen = True,
+    verbose = False,
+    generate_outputs=[
+        build_cfg.DataflowOutputType.ESTIMATE_REPORTS,
+        build_cfg.DataflowOutputType.BITFILE,
+        build_cfg.DataflowOutputType.PYNQ_DRIVER,
+        build_cfg.DataflowOutputType.DEPLOYMENT_PACKAGE,
+    ]
+    )
+    build.build_dataflow_cfg(model, cfg)
+    return
+
+if __name__ == '__main__':
+    model="models/p-enet.onnx"
+    output="output"
+    folding="configs/p-enet_m_config.json"
+    main(model, output, folding)
---
+++ hardware/custom_steps.py
@@ -0,0 +1,102 @@
+from onnx import TensorProto, helper
+from qonnx.core.datatype import DataType
+from qonnx.core.modelwrapper import ModelWrapper
+from qonnx.transformation.general import (  # ApplyConfig,
+    GiveReadableTensorNames,
+    GiveUniqueNodeNames,
+    SortGraph,
+    RemoveUnusedTensors,
+)
+from qonnx.transformation.infer_datatypes import InferDataTypes
+from qonnx.transformation.infer_shapes import InferShapes
+from qonnx.transformation.lower_convs_to_matmul import LowerConvsToMatMul
+from qonnx.transformation.infer_data_layouts import InferDataLayouts
+
+import finn.transformation.streamline.absorb as absorb
+import finn.transformation.fpgadataflow.convert_to_hw_layers as to_hw
+from finn.transformation.streamline import Streamline
+from finn.transformation.streamline.reorder import MoveScalarMulPastConvTranspose, MoveScalarLinearPastInvariants
+from finn.transformation.fpgadataflow.infer_pixel_padding_deconv import InferPixelPaddingDeconv
+from finn.transformation.streamline.reorder import MoveTransposePastJoinAdd, MoveTransposePastFork, MakeScaleResizeNHWC
+from finn.transformation.streamline.reorder import MoveMulPastFork, MoveLinearPastEltwiseAdd, MoveScalarMulPastConv, MoveScalarMulPastConvTranspose
+from finn.transformation.streamline.absorb import AbsorbMulIntoMultiThreshold
+from finn.transformation.streamline.round_thresholds import RoundAndClipThresholds
+from finn.builder.build_dataflow_steps import verify_step
+from finn.builder.build_dataflow_config import VerificationStepType
+
+
+from finn.builder.build_dataflow_config import DataflowBuildConfig
+
+
+def custom_step_streamline(model: ModelWrapper, cfg: DataflowBuildConfig):
+    """Run streamlining on given model. Streamlining involves moving floating point
+    scale/shift parameters around, collapsing adjacent ones into a single parameter,
+    then absorbing the scale/shift into the following `MultiThreshold` node.
+    Streamlining requires careful topology design and cannot be applied to all
+    topologies.
+    """
+
+    model = model.transform(absorb.AbsorbSignBiasIntoMultiThreshold())
+    model = model.transform(Streamline())
+    model = model.transform(MoveLinearPastEltwiseAdd())
+    model = model.transform(MoveMulPastFork())
+    model = model.transform(MoveScalarMulPastConv())
+    model = model.transform(MoveScalarMulPastConvTranspose())
+    model = model.transform(AbsorbMulIntoMultiThreshold())
+    model = model.transform(LowerConvsToMatMul())
+    model = model.transform(InferDataLayouts())
+    need_convtranspose = len(model.get_nodes_by_op_type("ConvTranspose")) > 0
+    if need_convtranspose:
+        model = model.transform(InferPixelPaddingDeconv())
+        model = model.transform(absorb.AbsorbTransposeIntoMultiThreshold())
+    model = model.transform(MoveScalarLinearPastInvariants())
+    model = model.transform(InferDataLayouts())
+    model = model.transform(MakeScaleResizeNHWC())
+    for i in range(18):
+        model = model.transform(absorb.AbsorbTransposeIntoMultiThreshold())
+        model = model.transform(MoveTransposePastJoinAdd())
+        model = model.transform(MoveTransposePastFork())
+        model = model.transform(absorb.AbsorbConsecutiveTransposes())
+    model = model.transform(Streamline())
+    model = model.transform(RoundAndClipThresholds())
+    model = model.transform(InferDataLayouts())
+    model = model.transform(RemoveUnusedTensors())
+
+    if VerificationStepType.STREAMLINED_PYTHON in cfg._resolve_verification_steps():
+        verify_step(model, cfg, "streamlined_python", need_parent=False)
+
+    return model
+
+
+def custom_step_convert_to_hw(model: ModelWrapper, cfg: DataflowBuildConfig):
+    """Convert eligible nodes to `HLSCustomOp` subclasses that represent HLS
+    layers. Which nodes and particular configurations can be converted to HLS
+    is limited, see the source code of the `convert_to_hw` module for more."""
+
+    model.set_tensor_datatype(model.graph.input[0].name, DataType["INT8"])
+    model = model.transform(InferDataLayouts())
+
+    # model = model.transform(DoubleToSingleFloat())
+    model = model.transform(InferDataTypes())
+    model = model.transform(to_hw.InferAddStreamsLayer())
+    model = model.transform(RoundAndClipThresholds())
+    # mem_mode = cfg.default_mem_mode.value
+    model = model.transform(to_hw.InferQuantizedMatrixVectorActivation())
+    model = model.transform(to_hw.InferThresholdingLayer())
+   
+    model = model.transform(to_hw.InferConvInpGen())
+            
+    model = model.transform(to_hw.InferUpsample())
+    
+    model = model.transform(to_hw.InferDuplicateStreamsLayer())
+    model = model.transform(absorb.AbsorbConsecutiveTransposes())
+    model = model.transform(absorb.AbsorbTransposeIntoMultiThreshold())
+    model = model.transform(RoundAndClipThresholds())
+    model = model.transform(GiveUniqueNodeNames())
+    model = model.transform(InferDataLayouts())
+    model = model.transform(InferDataTypes())
+    model = model.transform(InferShapes())
+    model = model.transform(GiveReadableTensorNames())
+    model = model.transform(RemoveUnusedTensors())
+    model = model.transform(SortGraph())
+    return model
