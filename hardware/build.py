167d81e21f4373d5fb36783d25c3075b8d8b7a48
Hugo LE BLEVEC <hugo.leblevec@gmail.com>
2025-03-04 01:30:46 +0100

    Adding default values to args

--- hardware/build.py
+++ hardware/build.py
@@ -30,7 +30,7 @@ def main(model, output, folding_config_file):
     output_dir          = output,
     synth_clk_period_ns = 3.3,
     target_fps          = 30,
-    board               = "TySOM-3A-ZU19EG",
+    board               = "ZCU104",
     shell_flow_type     = build_cfg.ShellFlowType.VIVADO_ZYNQ,
     steps               = build_dataflow_steps,
     folding_config_file = folding_config_file,
@@ -49,8 +49,8 @@ def main(model, output, folding_config_file):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-m', '--model', type=str, required=True, help='Path to ONNX model.', default='./models/p-enet_cityscapes.onnx')
-    parser.add_argument('-f', '--folding', type=str, required=True, help = 'Path to JSON folding config file.', default='./configs/p-enet_m_config.json')
+    parser.add_argument('-m', '--model', type=str, required=False, help='Path to ONNX model.', default='./models/p-enet_cityscapes.onnx')
+    parser.add_argument('-f', '--folding', type=str, required=False, help = 'Path to JSON folding config file.', default='./configs/p-enet_m_config.json')
     parser.add_argument('-o', '--output_path', type=str, help='Output path', default='output')
 
     args=parser.parse_args()
